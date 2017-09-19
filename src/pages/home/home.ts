bed9f734cf90b8d3e88d9212cefdd001043fd7f3
Kaartik <mathur.kaartik@gmail.com>
2017-09-19 00:20:43 -0400

    initial

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,3 +1,4 @@
+import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
@@ -6,9 +7,18 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+userData = null;
+  constructor(public navCtrl: NavController,
+              private facebook: Facebook) {
 
-  constructor(public navCtrl: NavController) {
+  }
 
+  loginFB(){
+  this.facebook.login(['email', 'public_profile' ]).then((response: FacebookLoginResponse)=> {
+    this.facebook.api('me?fields=id,name,email,first_name,picture.width(720).height(720).as(picture_large)', []).then(profile => {
+      this.userData = {email: profile['email'], first_name: profile['first_name'], picture: profile['picture_large'] ['data']['url'], username:profile['name']};
+    })
+  })
   }
 
 }
